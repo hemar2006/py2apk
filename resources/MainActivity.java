43572780eee2934d7a5b910b43a109e78d86d2a9
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-08-11 14:03:09 +0700

    Create MainActivity.java

---
+++ resources/MainActivity.java
@@ -0,0 +1,33 @@
+package ${package_name};
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    private WebView mWebView;
+
+    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+    public void onCreate(Bundle savedInstanceState) {        
+        setTheme(R.style.mainAppTheme); // add splash screen
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new MyWebViewClient());      
+        mWebView.loadUrl("${url_path}");       
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}

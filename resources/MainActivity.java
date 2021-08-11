e207e768ea02a070dd06ad6f20840cba3ed01d19
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-08-11 19:39:54 +0700

    Update MainActivity.java

--- resources/MainActivity.java
+++ resources/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
     private WebView mWebView;

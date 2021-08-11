f457260123d0727ad69b1a045c299ef0290b129e
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-08-11 20:40:34 +0700

    Update MainActivity.java

--- resources/MainActivity.java
+++ resources/MainActivity.java
@@ -14,8 +14,7 @@ public class MainActivity extends Activity {
     @Override
     @SuppressLint("SetJavaScriptEnabled")
     public void onCreate(Bundle savedInstanceState) {       
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);        
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {

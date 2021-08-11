25b1fc9dd7f23a3b8e8e4a70856b18c7b93aa698
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-08-11 23:33:52 +0700

    Update SplashActivity.java

--- resources/SplashActivity.java
+++ resources/SplashActivity.java
@@ -1,24 +1,22 @@
 package ${package_name};
 
-import android.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {    
 
-  private final int SPLASH_DISPLAY_DURATION = 3000;
-
-  @Override
-  public void onCreate(Bundle bundle) {
-      super.onCreate(bundle);
-      new Handler().postDelayed(new Runnable(){
-          @Override
-          public void run() {
-              Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-              SplashActivity.this.startActivity(mainIntent);
-              SplashActivity.this.finish();
-          }
-      }, SPLASH_DISPLAY_DURATION);
-  }
+    @Override    
+    public void onCreate(Bundle savedInstanceState) {       
+        super.onCreate(savedInstanceState);        
+	new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {                
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 2000);         
+    }
 }

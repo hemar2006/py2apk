94e13ac72f0624cde3eb3d946eac71b9d975486a
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-08-12 11:59:45 +0700

    Update MainActivity.java

--- resources/MainActivity.java
+++ resources/MainActivity.java
@@ -1,25 +1,42 @@
 package ${package_name};
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import android.view.View;
 
 public class MainActivity extends Activity {
     private WebView mWebView;
 
-    @Override
-    @SuppressLint("SetJavaScriptEnabled")
+    @Override   
     public void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);    
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyWebViewClient());      
-        mWebView.loadUrl("${url_path}");       
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        mWebView.loadUrl("${url_path}");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                mWebView.loadUrl(request.getUrl().toString());
+                return true;
+            }
+            
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                findViewById(R.id.splashscreen).setVisibility(View.GONE);
+                findViewById(R.id.activity_main_webview).setVisibility(View.VISIBLE);
+            }
+        }); 
     }
 
     @Override

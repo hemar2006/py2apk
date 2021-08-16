dda6c80abc518035a0fa72bc7903bd8840358cdb
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-08-16 15:55:38 +0700

    Update MainActivity.java

--- resources/MainActivity.java
+++ resources/MainActivity.java
@@ -3,10 +3,9 @@ package ${package_name};
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.webkit.WebSettings;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebResourceRequest;
 import android.view.View;
 import androidx.annotation.NonNull;
 import com.google.android.gms.ads.MobileAds;
@@ -45,20 +44,17 @@ public class MainActivity extends Activity {
                 createLoadInterstitial();
             }          
         }       
-        mWebView = findViewById(R.id.activity_main_webview);        
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setAllowFileAccessFromFileURLs(true);
-        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView.setInitialScale(1);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setVerticalScrollBarEnabled(false);      
         mWebView.loadUrl("${url_path}");
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                mWebView.loadUrl(request.getUrl().toString());
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
                 return true;
             }
                       
@@ -150,11 +146,11 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
             mWebView.goBack();
-        } else {
-            super.onBackPressed();
+            return true;
         }
+        return super.onKeyDown(keyCode, event);
     }
 }
